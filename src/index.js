f58669c69042793d0ad33c7570d62dc9d44764a9
kylejb <47502769+kylejb@users.noreply.github.com>
2020-08-08 12:30:39 -0400

    Deliverable: Challenge 3

--- src/index.js
+++ src/index.js
@@ -71,8 +71,63 @@ document.addEventListener("DOMContentLoaded", () => {
         dogBreedsUl.appendChild(frag)
     }
 
+    // Challenge 3
+
+    const clickHandler = () => {
+        const dogBreedsUl = document.querySelector("#dog-breeds")
+
+        dogBreedsUl.addEventListener("click", e => {
+            if (e.target.matches("#dog-breeds > li")) {
+                toggleColor(e.target);
+            }
+        })   
+        
+        dogBreedsUl.addEventListener("mouseover", e => {
+            if (e.target.matches("#dog-breeds > li")) {
+                toggleColor(e.target);
+            }
+            // reset the color after a short delay
+            setTimeout(function() {
+                e.target.style.color = "";
+            }, 500);
+        }, false)
+    }
+
+    const toggleColor = (eTarget) => {
+        switch (eTarget.style.color) {
+            case "purple":
+                eTarget.style.color = "";
+                break;
+            default:
+                eTarget.style.color = "purple"
+                break;
+        }
+    }   
+
+    // Challenge 4
+
     
+
     getDogImages();
     getDogBreeds();
+    clickHandler();
+
 })
 
+
+/* 
+
+Challenge 4--
+* enable filtering of index page based on dropbox
+    -- is this a form of "listener" based on the dropdown selection?
+    -- probably not formally, but conditional logic can be added... where? and how should this trigger the rendering of index?
+
+
+MISC TASKS IF EXTRA TIME:
+-- Expand the dropdown menu through by iterating from my exisiting list // or just iterate to complete alphabet and use conditional logic to filter
+-- Add hover color change to challenge 3's deliverable
+-- Should I (and how should I) expand the nested breed types to display on UL (e.g., should it prepend or append the main key or be it's own "breed-type")?
+-- CSS Formatting: is there an easy way to sync up the width and height of CSS flex?
+
+
+*/
